4bf8057df3407c5b183c8432cc57db17b62f1926
shadow-blade-X <jingxuansu1006@gmail.com>
2020-10-28 13:45:33 -0400

    Login Required
    Added login requirement and corresponding test, all test passed

--- flaskr-tdd/project/test/app_test.py
+++ flaskr-tdd/project/test/app_test.py
@@ -78,10 +78,14 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
-
+    
 def test_search(client):
     login(client, app.config["USERNAME"], app.config["PASSWORD"])
     client.post(
